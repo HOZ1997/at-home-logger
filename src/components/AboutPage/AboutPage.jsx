a10af3e76893b64f8c87acea75e8f39754eec1c8
Heather Osborne <theoz308@comcast.net>
2022-07-31 22:12:49 -0500

    added about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -12,18 +12,18 @@ function AboutPage() {
         <h2>At Home Logger</h2>
         <h3>Technologies Used</h3>
         <p>React Redux Node Express Postgres SQL CSS</p>
-        
         <h3>Thank you! </h3>
-        <h4>Dorian Cohort</h4>
-        <p>Dev</p>
-        <p> Prime Thank you for staring a part time program. </p>
-        <p>Prime Alumni Sarah and Alicia </p>
-        <p>Carter</p>
-        <h4>C.H. Robinson</h4>
-        <h4>Friends and Family. I am looking forward to more Auntie time </h4>
-        <h4>Whippa and Kal Pals</h4>
-        <h4>My friend Laura who said you have to do this! In memory of her brother Jason.  </h4>
-        
+        <h4>Dorian Cohort!!</h4>
+        <h4>Dev!</h4>
+        <p> Prime! Thank you for staring a part time program. </p>
+        <h4>Thank you for you ongoing support for my Prime journey! </h4>
+        <p>Prime Alumni Sarah and Alicia! </p><p>Carter and Zach!</p>
+        <p>C.H. Robinson!</p>
+        <p>Family and Friends!</p>
+        <p>Whippa and Kal Pals!</p>
+        <h4>Laura!</h4>
+        <p>Up Next for the project....</p>
+        <p> Here's Jonathan!</p>
       </div>
     </div>
   );
