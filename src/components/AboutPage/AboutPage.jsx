45bc1b34a1e4908d7ebdd4bc7439cc5b028788c8
Heather Osborne <theoz308@comcast.net>
2022-08-02 16:39:40 -0500

    udpates to About page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -16,17 +16,17 @@ function AboutPage() {
         <p>React Redux Node Express SQL CSS</p>
         
         <h3>Thank you! </h3>
-        <h4>Dorian Cohort!! and their families!</h4>
-        <h4>Dev!</h4>
+        <p>Dorian Cohort!! and their families!</p>
+        <p>Dev! and Casie! </p>
         <p> Prime! Thank you for staring a part time program. </p>
-        <h4>Thank you to the following for your ongoing support for my Prime journey!</h4>
+        <h4>Thank you to the following for your ongoing support during my Prime journey!</h4>
            <p>Prime Alumni - Sarah and Alicia!</p>
            <p>C.H. Robinson  - TMC, CHR IT, Carter, Zach, HatCHRy and Luke!</p>
         <p>Family - Mom and Dad - Laurel, Chris and Brandon - Derek, Kathryn, Ella and Palmer!</p>
         <p>Friends - Thank you for being understanding for all I have had to miss. </p>
         <p>Whippa and Kal Pals!</p>
-        <h4>Laura!</h4>
-        <h5>I dedicate this to Jason, Laura's brother. </h5>
+        <p>Laura!</p>
+        <p>I dedicate this to Jason, Laura's brother. </p>
         <p>Thank you all again! </p>
         <p>Now! Another amazing Dorian Cohort member! </p>
       </div>
