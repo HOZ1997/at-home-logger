e62ad1e4c8ec1206153b779e2902051d6d4471b6
Heather Osborne <theoz308@comcast.net>
2022-08-02 12:40:16 -0500

    updates to about

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -21,12 +21,13 @@ function AboutPage() {
         <p> Prime! Thank you for staring a part time program. </p>
         <h4>Thank you to the following for your ongoing support for my Prime journey!</h4>
            <p>Prime Alumni - Sarah and Alicia!</p>
-           <p>C.H. Robinson  - My TMC team, Carter, Zach, Luke and HatCHRy!</p>
-        <p>Family - Mom, Dad, Laurel, Chris, Derek, Kathryn, Ella, Brandon and Palmer!</p>
+           <p>C.H. Robinson  - TMC, CHR IT, Carter, Zach, HatCHRy and Luke!</p>
+        <p>Family - Mom and Dad - Laurel, Chris and Brandon - Derek, Kathryn, Ella and Palmer!</p>
         <p>Friends - Thank you for being understanding for all I have had to miss. </p>
         <p>Whippa and Kal Pals!</p>
         <h4>Laura!</h4>
         <h5>I dedicate this to Jason, Laura's brother. </h5>
+        <p>Thank you all again! </p>
         <p>Now! Another amazing Dorian Cohort member! </p>
       </div>
     </div>
